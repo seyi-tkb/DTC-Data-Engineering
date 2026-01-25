5e848b2627232104c3e0154c05764e7c98070212
Oluwaseyi Ogundimu <105808404+seyi-tkb@users.noreply.github.com>
2026-01-25 17:47:06 +0000

    q4-6

--- README.md
+++ README.md
@@ -24,7 +24,6 @@ postgres:5432
 #### Question 3. Counting short trips
 
 ```sql
-
 SELECT 
 	COUNT(lpep_pickup_datetime) pickup_count
 FROM 
@@ -34,4 +33,75 @@ WHERE
 	AND 
 		trip_distance <= 1;
 ```
+
+#### Question 4. Longest trip for each day
+```sql
+SELECT
+	DATE_TRUNC('day', lpep_pickup_datetime) pickup_day,
+	MAX(trip_distance)
+FROM
+	green_taxi
+WHERE
+	trip_distance <100
+GROUP BY
+	1
+ORDER BY
+	2 DESC
+LIMIT 1;
+```
+
+#### Question 5. Biggest pickup zone
+```sql
+SELECT
+	tz."Zone",
+	COUNT(*)
+FROM
+	green_taxi gt
+JOIN 
+	taxi_zone tz
+ON 
+	tz."LocationID" = gt."PULocationID"
+WHERE 
+	date_trunc('day', gt."lpep_pickup_datetime") = '2025-11-18'
+GROUP BY
+	1
+ORDER BY
+	2 DESC
+LIMIT 1;
+```
+
+#### Question 6. Largest tip
+```sql
+WITH harlem as (
+SELECT
+	tz."Zone", 
+	gt."DOLocationID",
+	MAX(gt."tip_amount") max_tip
+FROM
+	green_taxi gt
+JOIN 
+	taxi_zone tz
+ON 
+	tz."LocationID" = gt."PULocationID"
+WHERE 
+	tz."Zone" = 'East Harlem North' 
+	AND
+	DATE_TRUNC('month', gt."lpep_pickup_datetime") = DATE_TRUNC('month', DATE '2025-11-01')
+GROUP BY 1, 2
+)
+
+SELECT 
+	tz."Zone", 
+	h."max_tip"
+FROM
+	harlem h
+JOIN 
+	taxi_zone tz
+ON 
+	tz."LocationID" = h."DOLocationID"
+ORDER BY 
+	2 DESC
+LIMIT 1;
+```
+
 
--- homework_1/question_3.sql
+++ homework_1/question_3.sql
@@ -10,4 +10,78 @@ WHERE
 	AND 
 		trip_distance <= 1;
 
--- Ans. 8007
+-- Ans. 8007 (B)
+
+
+-- Question 4. Longest trip for each day
+-- Which was the pick up day with the longest trip distance? Only consider trips with trip_distance less than 100 miles.
+
+SELECT
+	DATE_TRUNC('day', lpep_pickup_datetime) pickup_day,
+	MAX(trip_distance)
+FROM
+	green_taxi
+WHERE
+	trip_distance <100
+GROUP BY
+	1
+ORDER BY
+	2 DESC
+LIMIT 1;
+
+-- Ans. 2025-11-14 (A)
+
+-- Question 5. Biggest pickup zone
+-- 
+SELECT
+	tz."Zone",
+	COUNT(*)
+FROM
+	green_taxi gt
+JOIN 
+	taxi_zone tz
+ON 
+	tz."LocationID" = gt."PULocationID"
+WHERE 
+	date_trunc('day', gt."lpep_pickup_datetime") = '2025-11-18'
+GROUP BY
+	1
+ORDER BY
+	2 DESC
+LIMIT 1;
+
+-- Ans. East Harlem North (A)
+
+-- Question 6. Largest tip
+-- For the passengers picked up in the zone named "East Harlem North" in November 2025, which was the drop off zone that had the largest tip?
+WITH harlem as (
+SELECT
+	tz."Zone", 
+	gt."DOLocationID",
+	MAX(gt."tip_amount") max_tip
+FROM
+	green_taxi gt
+JOIN 
+	taxi_zone tz
+ON 
+	tz."LocationID" = gt."PULocationID"
+WHERE 
+	tz."Zone" = 'East Harlem North' 
+	AND
+	DATE_TRUNC('month', gt."lpep_pickup_datetime") = DATE_TRUNC('month', DATE '2025-11-01')
+GROUP BY 1, 2
+)
+
+SELECT 
+	tz."Zone", 
+	h."max_tip"
+FROM
+	harlem h
+JOIN 
+	taxi_zone tz
+ON 
+	tz."LocationID" = h."DOLocationID"
+ORDER BY 
+	2 DESC
+LIMIT 1;
+-- Ans. Yorkville West (B)
